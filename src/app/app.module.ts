8c560d9e7d1678c75b996a8bc86d2e45cadf5a35
kenzo <alarm88@mail.ru>
2019-12-11 23:03:20 +0400

    Начало добавления формы ввода события

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ViewComponent } from './view/view.component';
 import { DayComponent } from './day/day.component';
 import {FormsModule} from '@angular/forms';
+import { AddEventComponent } from './add-event/add-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewComponent,
-    DayComponent
+    DayComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule,
