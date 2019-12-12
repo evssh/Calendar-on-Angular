dfe79fa491ae4905a33b8bf900973576054f9021
kenzo <alarm88@mail.ru>
2019-12-12 22:44:11 +0400

    show event per day in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-export interface Event {
+export interface EventMy {
   date: Date
   title: string
   text: string
@@ -15,9 +15,10 @@ export interface Event {
 })
 export class AppComponent {
   selectDay: Date
-  events: Event[] = [
+  events: EventMy[] = [
     {date: new Date(), title: 'Event test', text: 'Loren 10', id: 1},
-    {date: new Date('December 18, 2019 00:00:00'), title: 'Dead line', text: 'Aaaaaaa!', id: 2}
+    {date: new Date(), title: 'Event test2', text: 'Loren 11', id: 2},
+    {date: new Date('December 18, 2019 00:00:00'), title: 'Dead line', text: 'Aaaa!', id: 3},
   ]
 
   chooseDate(date) {
--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -1,29 +1,45 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {EventMy} from "../app.component";
 
 @Component({
   selector: 'app-day',
   templateUrl: './day.component.html',
   styleUrls: ['./day.component.scss']
 })
-export class DayComponent implements OnInit {
+export class DayComponent implements OnInit, OnChanges {
 
   @Input() day
-  @Input() events
+  @Input() eventsMy: EventMy[]
+  // @Input() eventElement: EventMy
 
   hours: Array<number> = []
   tempArrEvent: Event[] = []
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.eventInDay()
+  }
+
   ngOnInit() {
     this.pushHours()
-    console.log(this.events)
+    console.log(this.eventsMy)
+    // console.log('ffff', this.day)
+    // if (this.day) {this.eventInDay()}
   }
 
   eventInDay() {
-    for (let i = 0; i < this.events.length; i++) {
-
+    // console.log('ddd')
+    this.tempArrEvent = []
+    for (let i = 0; i < this.eventsMy.length; i++) {
+      // console.log(this.eventsMy[i].date === this.day)
+      if (this.eventsMy[i].date.setHours(0,0,0,0) === this.day.setHours(0,0,0,0)) {
+        // console.log('events: ', this.eventsMy[i].title)
+        this.tempArrEvent.push(this.eventsMy[i])
+      }
     }
+    console.log('tempArEvent',this.tempArrEvent)
+
   }
 
   pushHours() {
